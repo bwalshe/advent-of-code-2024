8e2e0343e06989931be9b729a1e98e1fbc197188
Brian Walshe <walshe.brian@gmail.com>
2024-12-02 23:12:55 +0000

    cleaned up the implementation of the monotonic check

--- app/Day2.hs
+++ app/Day2.hs
@@ -18,26 +18,28 @@ rowParser = sepBy L.decimal (char ' ')
 dataParser :: Parser [[Int]]
 dataParser = sepBy rowParser newline <* eof
 
+data Monotonic = Inc Int | Dec Int | Fail deriving (Eq)
+
 monotonic :: [Int] -> Bool
-monotonic (a : b : t)
-  | a < b && b <= a + 3 = increasing (b : t)
-  | a > b && a <= b + 3 = decreasing (b : t)
-  | otherwise = False
-monotonic [_] = True
-monotonic [] = True
-
-increasing :: [Int] -> Bool
-increasing (a : b : t) = a < b && b <= a + 3 && increasing (b : t)
-increasing [_] = True
-increasing [] = True
-
-decreasing :: [Int] -> Bool
-decreasing (a : b : t) = a > b && a <= b + 3 && decreasing (b : t)
-decreasing [_] = True
-decreasing [] = True
+monotonic (a : b : t) = foldl step start t /= Fail
+  where
+    isInc a' b' = a' < b' && b' <= a' + 3
+    isDec a' b' = a' > b' && a' <= b' + 3
+    start
+      | isInc a b = Inc b
+      | isDec a b = Dec b
+      | otherwise = Fail
+    step (Inc a') b'
+      | isInc a' b' = Inc b'
+      | otherwise = Fail
+    step (Dec a') b'
+      | isDec a' b' = Dec b'
+      | otherwise = Fail
+    step Fail _ = Fail
+monotonic _ = False
 
 task1 :: [[Int]] -> Int
-task1 lists = sum $ map (fromEnum . monotonic) $ filter (not . null) lists
+task1 = sum . map (fromEnum . monotonic)
 
 dampenedMonotonic :: [Int] -> Bool
 dampenedMonotonic nums = monotonic nums || any monotonic sublists
@@ -46,14 +48,14 @@ dampenedMonotonic nums = monotonic nums || any monotonic sublists
     sublists = [exclude i nums | i <- [0 .. length nums - 1]]
 
 task2 :: [[Int]] -> Int
-task2 lists = sum $ map (fromEnum . dampenedMonotonic) $ filter (not . null) lists
+task2 = sum . map (fromEnum . dampenedMonotonic)
 
 runDay2Tasks :: IO ()
 runDay2Tasks = do
   let fileName = "data/day2/input.txt"
   fileText <- TIO.readFile fileName
   case runParser dataParser fileName fileText of
-    Right lists -> do 
-       printf "Task 1: %d\n" $ task1 lists
-       printf "Task 2: %d\n" $ task2 lists
+    Right lists -> do
+      printf "Task 1: %d\n" $ task1 lists
+      printf "Task 2: %d\n" $ task2 lists
     Left e -> print $ errorBundlePretty e
