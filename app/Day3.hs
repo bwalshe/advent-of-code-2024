fef230702383b5b4f5f3e27bb25f2f02f19be4d1
Brian Walshe <walshe.brian@gmail.com>
2024-12-03 21:49:16 +0000

    Using a fancy State monad to do the command evaluation

--- app/Day3.hs
+++ app/Day3.hs
@@ -2,10 +2,11 @@
 
 module Day3 where
 
-import Data.Text
+import Control.Monad.State
+import Data.Text hiding (last)
 import Data.Text.IO as TIO
 import Data.Void
-import Text.Megaparsec as M
+import Text.Megaparsec as M hiding (State)
 import Text.Megaparsec.Char as MC
 import Text.Megaparsec.Char.Lexer as L
 import Text.Printf
@@ -52,15 +53,32 @@ task1 = sum . fmap act
       Mul (a, b) -> a * b
       _ -> 0
 
+evaluateCmd :: Command -> State (Bool, Int) Int
+evaluateCmd cmd = do
+  (active, val) <- get
+  case cmd of
+    Do -> do
+      put (True, val)
+      return val
+    Dont -> do
+      put (False, val)
+      return val
+    Mul (a, b) -> do
+      let val' =
+            if active
+              then val + a * b
+              else val
+      put (active, val')
+      return val'
+
+evaluateCmds :: [Command] -> State (Bool, Int) Int
+evaluateCmds cmds = last <$> traverse evaluateCmd cmds
+-- Note to self: I don't like the way I am using traverse and then 
+-- fmap last, I think foldM is what I need, but couldn't get it to
+-- work
+
 task2 :: [Command] -> Int
-task2 = second . Prelude.foldl act (True, 0)
-  where
-    act state cmd = case (state, cmd) of
-      ((True, val), Mul (a, b)) -> (True, val + a * b)
-      ((_, val), Do) -> (True, val)
-      ((_, val), Dont) -> (False, val)
-      _ -> state
-    second (_, b) = b
+task2 cmds = evalState (evaluateCmds cmds) (True, 0)
 
 runDay3Tasks :: IO ()
 runDay3Tasks = do
@@ -68,7 +86,7 @@ runDay3Tasks = do
   fileText <- TIO.readFile fileName
   case runParser dataParser fileName fileText of
     Right (cmds, rest) -> do
-      printf "remainder: %s\n" rest
+      printf "Unparsed remainder: %s\n" rest
       printf "Last command: %s\n" $ show $ Prelude.last cmds
       printf "Task 1: %d\n" $ task1 cmds
       printf "Task 2: %d\n" $ task2 cmds
